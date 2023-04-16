7eec9ef36a14ea1a843fb7a94e995f06c9b02b87
Devesh Kumar <devesh2027@gmail.com>
2023-04-16 19:38:03 +0530

    app: Added handlers to save file on window closing

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const { app, BrowserWindow, nativeTheme } = require("electron");
+const {
+	app,
+	dialog,
+	BrowserWindow,
+	nativeTheme,
+	ipcMain,
+} = require("electron");
 const path = require("path");
 
 const setMenu = require("./handlers/set-menu");
+const { saveFile } = require("./handlers/file-ops");
+
+let userHasUnsavedChanges = false;
+let selectedFilePath = null;
 
 const createWindow = () => {
 	const mainWindow = new BrowserWindow({
@@ -15,6 +25,40 @@ const createWindow = () => {
 	nativeTheme.themeSource = "light";
 	mainWindow.loadURL(path.resolve(__dirname, "./src/index.html"));
 	setMenu();
+
+	mainWindow.on("close", (event) => {
+		if (!userHasUnsavedChanges) return;
+		const choice = dialog.showMessageBoxSync(mainWindow, {
+			type: "question",
+			buttons: ["Save", "Don't Save", "Cancel"],
+			title: "Confirm",
+			message: "Do you want to save changes to your file?",
+		});
+		if (choice === 0) {
+			event.preventDefault();
+			mainWindow.webContents.send("get-entered-contents");
+			ipcMain.on("receive-entered-contents", async (_event, contents) => {
+				// We have the contents for the file entered by the user.
+				// Open a save dialog
+				if (selectedFilePath) {
+					// Store the contents to an existing file.
+					saveFile(selectedFilePath, contents);
+				} else {
+					const selectedPath = dialog.showSaveDialogSync(mainWindow);
+					saveFile(selectedPath, contents);
+				}
+
+				// Now done, close the window.
+				mainWindow.close();
+			});
+		}
+		if (choice === 2) event.preventDefault();
+		else userHasUnsavedChanges = false;
+	});
+
+	ipcMain.on("set-unsaved-changes", (hasUnsavedChanges) => {
+		userHasUnsavedChanges = hasUnsavedChanges;
+	});
 };
 
 // This method will be called when Electron has finished
--- preload.js
+++ preload.js
@@ -1,8 +1,18 @@
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 const { isWindows, isMac, isLinux } = require("./detect-os");
 
 contextBridge.exposeInMainWorld("electron", {
 	isWindows,
 	isLinux,
 	isMac,
+	setHasUnsavedChanges: () => {
+		ipcRenderer.send("set-unsaved-changes");
+	},
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+	ipcRenderer.on("get-entered-contents", (event) => {
+		const value = document.getElementById("root").value;
+		event.sender.send("receive-entered-contents", value);
+	});
 });
