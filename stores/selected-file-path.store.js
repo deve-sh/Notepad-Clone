ca8aabe4125c5901a9b2d11bd11f37698ea11ada
Devesh Kumar <devesh2027@gmail.com>
2023-04-16 21:00:24 +0530

    electron: Integrate file path update signal to store

--- stores/selected-file-path.store.js
+++ stores/selected-file-path.store.js
@@ -1,8 +1,13 @@
+const signalUpdatedFilePath = require("../utils/signal-updated-file-path");
+
 // Common closure function to store the selected file in memory.
 const selectedFileStore = () => {
 	let selectedFilePath = "";
 	return {
-		set: (path) => (selectedFilePath = path),
+		set: (path) => {
+			selectedFilePath = path;
+			signalUpdatedFilePath(path);
+		},
 		get: () => selectedFilePath,
 	};
 };
