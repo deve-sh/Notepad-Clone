8a7681bca75b8d950513bf5223bfbf44762679bc
Devesh Kumar <devesh2027@gmail.com>
2023-04-16 20:59:39 +0530

    app:  Add events for file path and unsaved changes indicator

--- preload.js
+++ preload.js
@@ -15,4 +15,25 @@ window.addEventListener("DOMContentLoaded", () => {
 		const value = document.getElementById("root").value;
 		event.sender.send("receive-entered-contents", value);
 	});
+
+	// Update document title on selected file path change
+	ipcRenderer.on("selected-file-change", (_event, fileName) => {
+		document.title = `${fileName
+			.split(/[\\/]/g)
+			.pop()
+			.split(".")
+			.slice(0, -1)
+			.join(".")} - Notepad`;
+	});
+
+	// Update text area's content
+	ipcRenderer.on("file-content-change", (_event, content) => {
+		document.getElementById("root").value = content;
+	});
+
+	// Remove unsaved changes indicator from title
+	ipcRenderer.on("save-complete", () => {
+		window.hasUnsavedChanges = false;
+		document.title = document.title.replace("*", "");
+	});
 });
